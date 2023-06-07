2ba5524e9ee5b816b54d4975c4528591d2ebb357
shuhanasherenp <84239548+shuhanasherenp@users.noreply.github.com>
2023-06-07 02:35:23 -0700

    Add files via upload

---
+++ calc.sh
@@ -0,0 +1,13 @@
+echo "enter first number:"
+read a
+echo "enter second number:"
+read b
+sum=$((a + b))
+diff=$((a - b))
+mul=$((a * b))
+div=$((a / b))
+echo "sum:" $sum
+echo "difference:" $diff
+echo "product:" $mul
+echo "quotient:" $div
+
